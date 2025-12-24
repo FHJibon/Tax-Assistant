8a9023a8c61bc85fa466167d0392ec779023159d
FHJibon <f.h.jibon1250@gmail.com>
2025-12-24 06:59:52 +0600

    Fixed Issue

--- Backend/app/main.py
+++ Backend/app/main.py
@@ -23,7 +23,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:3000",
         "http://127.0.0.1:3000",
-        "https://ai-tax-assistant-bd.vercel.app/"
+        "https://ai-tax-assistant-bd.vercel.app"
     ],
     allow_credentials=True,
     allow_methods=["*"],
